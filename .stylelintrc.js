8c3f9a69e4369fd853944bc260ad96ae6fb0321d
CaoMeiYouRen <996881204@qq.com>
2021-02-04 23:17:16 +0800

    chore: init

---
+++ .stylelintrc.js
@@ -0,0 +1,17 @@
+module.exports = {
+    extends: [
+        'stylelint-config-sass-guidelines',
+        'stylelint-config-cmyr',
+    ],
+    plugins: [
+        'stylelint-scss',
+        'stylelint-order',
+    ],
+    rules: {
+        'scss/at-import-partial-extension-blacklist': null,
+        'max-nesting-depth': null,
+        'selector-max-compound-selectors': null,
+        'selector-class-pattern': null,
+        'order/properties-alphabetical-order': null
+    },
+}
